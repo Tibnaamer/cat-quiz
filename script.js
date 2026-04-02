56fd2c0438687ebcbe310050d412f6a66225db1a
Tibnaamer <tibnaamer@gmail.com>
2026-04-02 12:54:13 +0100

    Added and amended questions
    Added and amended questions

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const questions = [
+  {
+    question: "Who has more bones in their body?",
+    answers: [
+      { text: "Cats", correct: true },
+      { text: "Humans", correct: false },
+      { text: "Rabbits", correct: false },
+      { text: "Hamsters", correct: false },
+    ],
+  },
+    {
+    question: "On average, how many hours a day do cats sleep?",
+    answers: [
+      { text: "Less than 4 hours", correct: false },
+      { text: "5 - 11 hours", correct: false },
+      { text: "12 - 16 hours", correct: false },
+      { text: "More than 16 hours", correct: true },
+    ],
+  }
+    {
+    question: "Which domesticated Cat breed is the largest?",
+    answers: [
+      { text: "Ragdoll", correct: false },
+      { text: "Maine Coon", correct: true },
+      { text: "Siamese", correct: false },
+      { text: "Balinese", correct: false },
+    ],
+  }
+     {
+    question: "Who is commonly credited with inventing the Cat flap?",
+    answers: [
+      { text: "Freddie Mercury", correct: false },
+      { text: "Sir Isaac Newton", correct: true },
+      { text: "Benjamin Franklin", correct: false },
+      { text: "Charles Darwin", correct: false },
+    ],
+  }
+      {
+    question: "whose heart beats faster?",
+    answers: [
+      { text: "Cats", correct: false },
+      { text: "Humans", correct: false },
+      { text: "Dogs", correct: false },
+      { text: "Hamster ", correct: true },
+    ],
+  }
+        {
+    question: "On average, how long do Cats live?",
+    answers: [
+      { text: "Around 5 years", correct: false },
+      { text: "Around 10 years", correct: false },
+      { text: "Around 15 years", correct: true },
+      { text: "Around 25 years ", correct: false },
+    ],
+  }
+          {
+    question: "What is a group of Cats called?",
+    answers: [
+      { text: "Clowder ", correct: true},
+      { text: "Clawers ", correct: false },
+      { text: "Cloup", correct: false },
+      { text: "Clapers", correct: false },
+    ],
+  }
+            {
+    question: "What is the top speed of a house cat?",
+    answers: [
+      { text: "0 - 6 mph ", correct: false},
+      { text: "7 - 13 mph ", correct: false },
+      { text: "14 - 19 mph", correct: false },
+      { text: "20mph and above", correct: true },
+    ],
+  }
+];
